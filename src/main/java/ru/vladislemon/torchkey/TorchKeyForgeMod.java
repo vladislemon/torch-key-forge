33f8bae1df63f56890ca0c6405b7cbe07ef212b8
vladislemon <vladislemon@gmail.com>
2020-11-26 13:01:25 +0300

    Make sure the mod being absent on the other network side does not cause the client to display the server as incompatible

--- src/main/java/ru/vladislemon/torchkey/TorchKeyForgeMod.java
+++ src/main/java/ru/vladislemon/torchkey/TorchKeyForgeMod.java
@@ -4,9 +4,13 @@ import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.client.util.InputMappings;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 import org.lwjgl.glfw.GLFW;
 
 @Mod("torch-key-forge")
@@ -20,6 +24,13 @@ public class TorchKeyForgeMod {
 
     public TorchKeyForgeMod() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onClientSetupEvent);
+        //Make sure the mod being absent on the other network side does not cause the client to display the server as incompatible
+        ModLoadingContext
+                .get()
+                .registerExtensionPoint(
+                        ExtensionPoint.DISPLAYTEST,
+                        () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true)
+                );
     }
 
     @OnlyIn(Dist.CLIENT)
